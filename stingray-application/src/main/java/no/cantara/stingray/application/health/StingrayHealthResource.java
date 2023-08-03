ae334f8c0e4b0ac6f76da4be4b93b1cf138e9a54
baardl <bard.lind@gmail.com>
2023-08-03 12:46:26 +0200

    #272 Provide pretty json on http://<host>:<port>/<context>/health/pretty

--- stingray-application/src/main/java/no/cantara/stingray/application/health/StingrayHealthResource.java
+++ stingray-application/src/main/java/no/cantara/stingray/application/health/StingrayHealthResource.java
@@ -1,6 +1,7 @@
 package no.cantara.stingray.application.health;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityRequirements;
@@ -47,6 +48,38 @@ public class StingrayHealthResource {
             health.put("health-compute-time-ms", String.valueOf(healthComputeTimeMs));
             health.put("health-updater-thread-alive", String.valueOf(activelyUpdatingCurrentHealth));
             return Response.status(Response.Status.OK).entity(health).build();
+        } catch (Throwable t) {
+            log.error("While getting health", t);
+            ObjectNode health = mapper.createObjectNode();
+            health.put("Status", "FAIL");
+            health.put("errorMessage", "While getting health");
+            StringWriter strWriter = new StringWriter();
+            t.printStackTrace(new PrintWriter(strWriter));
+            health.put("errorCause", strWriter.toString());
+            return Response.status(Response.Status.OK).entity(health).build();
+        }
+    }
+
+    @GET
+    @Path("/pretty")
+    @Produces(MediaType.APPLICATION_JSON)
+    @SecurityRequirements(@SecurityRequirement(name = "none")) // disable authorization requirement in openapi spec
+    public Response getHealthPrettyPrinted() {
+        try {
+            String currentHealthJsonWithoutTimestamp = healthService.getCurrentHealthJson();
+            ObjectNode health = (ObjectNode) mapper.readTree(currentHealthJsonWithoutTimestamp);
+            long healthComputeTimeMs = healthService.getHealthComputeTimeMs();
+            boolean activelyUpdatingCurrentHealth = healthService.isActivelyUpdatingCurrentHealth();
+            if (!activelyUpdatingCurrentHealth) {
+                health.put("Status", "FAIL");
+                health.put("errorMessage", "health-updater-thread is dead.");
+            }
+            health.put("now", Instant.now().toString());
+            health.put("health-compute-time-ms", String.valueOf(healthComputeTimeMs));
+            health.put("health-updater-thread-alive", String.valueOf(activelyUpdatingCurrentHealth));
+            mapper.enable(SerializationFeature.INDENT_OUTPUT);
+            String prettyHealth = mapper.writeValueAsString(health);
+            return Response.status(Response.Status.OK).entity(prettyHealth).build();
         } catch (Throwable t) {
             log.error("While getting health", t);
             ObjectNode health = mapper.createObjectNode();
